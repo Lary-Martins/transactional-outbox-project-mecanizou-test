1da956affc58115e7ecbce93b6cb6bf964bf82cb
Laryssa Martins <laryssamartinsoffice@gmail.com>
2025-12-18 23:50:41 -0300

    feat: applies the Singleton pattern for database connection.

---
+++ src/database/prismaConnect.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+
+class Database {
+  private static instance: PrismaClient;
+
+  private constructor() {};
+
+  public static getInstance(): PrismaClient {
+    if (!Database.instance) {
+      Database.instance = new PrismaClient({
+        log: ['query', 'info', 'warn', 'error'],
+      });
+    }
+    return Database.instance;
+  }
+}
+
+export const prismaConnect = Database.getInstance();
